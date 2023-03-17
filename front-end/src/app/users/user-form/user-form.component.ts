4f714ab6eb364a1c48a40c853a42fa592384c133
Saad Beidouri <saadbeidouri@Saads-MacBook-Air.local>
2023-03-17 09:51:53 +0100

    #2 affiche de user-form

--- front-end/src/app/users/user-form/user-form.component.ts
+++ front-end/src/app/users/user-form/user-form.component.ts
@@ -1,29 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
-
-import { UserService } from '../../../services/user.service';
-import { User } from '../../../models/user.models';
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-form',
   templateUrl: './user-form.component.html',
   styleUrls: ['./user-form.component.scss']
 })
-export class UserFormComponent implements OnInit {
+export class UserFormComponent {
+  userForm: FormGroup;
 
-  public userForm: FormGroup;
-
-  constructor(public formBuilder: FormBuilder, public userService: UserService) {
-    this.userForm = this.formBuilder.group({
-      firstName: [''],
-      lastName: ['']
+  constructor() {
+    this.userForm = new FormGroup({
+      firstName: new FormControl('', Validators.required),
+      lastName: new FormControl('', Validators.required)
     });
   }
 
-  ngOnInit(): void {
-  }
-
-  addUser(): void {
-    // We retrieve here the user object from the userForm and we cast the type "as User".
+  addUser() {
+    // Ajouter un nouvel utilisateur ici
   }
 }
