2f28258219d552864f8152090f60632900f225a5
komi-assimpah <komi-jean-paul.assimpah@etu.unice.fr>
2023-03-18 21:02:19 +0100

    creation des component GameAnswerComponet #5, GamesQuestionComponent #4, GamePageComponent #3

---
+++ front-end/src/app/gameAnswerComponent/gameAnswer.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'gameAnswer',
+    templateUrl: 'gameAnswer.html',
+    styleUrls: ['./gameAnswer.scss']
+})
+
+export class gameAnswerComponent implements OnInit {
+    constructor() {}
+    ngOnInit(): void {}
+} 
