1266ddb75db0b5e54602277c0e901df3a87535ca
Saad Beidouri <saadbeidouri@Saads-MacBook-Air.local>
2023-03-27 11:52:25 +0200

    pop-up

--- front-end/src/app/config-pop-up/config-pop-up.component.ts
+++ front-end/src/app/config-pop-up/config-pop-up.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef} from '@angular/core';
+import {User} from "../../models/user.models";
+import {ActivatedRoute} from "@angular/router";
+import {UserService} from "../../services/user.service";
 
 @Component({
   selector: 'app-config-pop-up',
@@ -6,5 +9,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./config-pop-up.component.scss']
 })
 export class ConfigPopUpComponent {
+  user: User = {id:'',firstName:'',lastName:''};
+
+  constructor(private route: ActivatedRoute, private userService: UserService,private elementRef:ElementRef) {}
+
+  ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id')!;
+    this.user = this.userService.getUserById(id);
+  }
+
+  value = 0;
+  updateValue() {
+    if (this.value < -50) {
+      this.value = -50;
+    } else if (this.value > 50) {
+      this.value = 50;
+    }
+  }
+
+  changeFontSize() {
+    const textElements: NodeListOf<HTMLElement> = this.elementRef.nativeElement.querySelectorAll('.resize');
+    textElements.forEach((element: HTMLElement) => {
+      const newValue =  30*(this.value/100+1);
+      console.log(newValue);
+      element.style.fontSize = newValue + 'px';
+    });
+    const button:HTMLElement = this.elementRef.nativeElement.querySelectorAll('.button-card');
+    const newValue =  30*(this.value/100+1);
+    console.log(newValue);
+    button.style.fontSize = newValue + 'px';
+  }
+
 
 }
