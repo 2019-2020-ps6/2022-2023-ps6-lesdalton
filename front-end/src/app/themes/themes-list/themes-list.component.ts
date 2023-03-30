92bffb027be109279719c55e4bcb667f58878fc5
Jamal Eddine El Betioui <jamal-eddine.el-betioui@etu.unice.fr>
2023-03-30 09:55:35 +0200

    buttons

--- front-end/src/app/themes/themes-list/themes-list.component.ts
+++ front-end/src/app/themes/themes-list/themes-list.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {Theme} from "../../../models/theme.models";
 import {THEME_LIST} from "../../../mocks/theme.mocks";
+import {ThemeService} from "../../../services/theme.service";
 
 @Component({
   selector: 'app-themes-list',
@@ -9,4 +10,11 @@ import {THEME_LIST} from "../../../mocks/theme.mocks";
 })
 export class ThemesListComponent {
   public themes:Theme[] = THEME_LIST;
+
+  constructor(private themeService:ThemeService) {
+  }
+
+  ngOnInit() {
+    this.themeService.themes$.subscribe((themes) => (this.themes = themes));
+  }
 }
