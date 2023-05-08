6dcfd44187998d8ac8fc00a9c3dc0e721d0e5bab
Jamal Eddine El Betioui <jamal-eddine.el-betioui@etu.unice.fr>
2023-05-08 12:03:19 +0200

    result page

--- front-end/src/app/game/game-result/game-result.component.ts
+++ front-end/src/app/game/game-result/game-result.component.ts
@@ -18,12 +18,8 @@ export class GameResultComponent {
   getResultMessage(): string {
     if (this.playerScore === 0) {
       return "Oups ! Vous n'avez obtenu aucune réponse correcte.";
-    } else if (this.playerScore < 5) {
-      return `Pas mal ! Vous avez obtenu ${this.playerScore} réponses correctes.`;
-    } else if (this.playerScore < 10) {
-      return `Bravo ! Vous avez obtenu ${this.playerScore} réponses correctes.`;
     } else {
-      return `Félicitations ! Vous avez obtenu un score parfait de ${this.playerScore} réponses correctes !`;
+      return `Bravo ! Vous avez obtenu ${this.playerScore} réponses correctes.`;
     }
   }
 
