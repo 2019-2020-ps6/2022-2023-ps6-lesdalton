e542d287e851ef9a4d91b9cb5c1f1e613eb922ab
Saad Beidouri <saadbeidouri@Saads-MacBook-Air.local>
2023-05-15 11:05:54 +0200

    init backEnd

---
+++ backend/app/models/user.model.js
@@ -0,0 +1,8 @@
+
+const Joi = require('joi')
+const BaseModel = require('../utils/base-model.js')
+
+module.exports = new BaseModel('User', {
+  firstName: Joi.string().required(),
+  lastName: Joi.string().required(),
+})
