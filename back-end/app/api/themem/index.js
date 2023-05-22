dc424cbb46d1456b31345c5530daba70f5db6b12
komi-assimpah <komi-jean-paul.assimpah@etu.unice.fr>
2023-05-22 10:50:59 +0200

    theme

--- back-end/app/api/themem/index.js
+++ back-end/app/api/themem/index.js
@@ -7,15 +7,44 @@ const router = new Router()
 
 ///////////////////////////////////
 //
+//    CREER UN THEME
+//
+///////////////////////////////////
+router.post('/', (req, res) => {
+  try {
+    const theme = Theme.create({ ...req.body })
+    res.status(201).json(theme)
+  } catch (err) {
+    manageAllErrors(res, err)
+  }
+})
+
+///////////////////////////////////
+//
 //    AFFICHER TOUS LES THEMES
 //
 ///////////////////////////////////
 router.get('/', (req, res) => {
     try {
-      res.status(200).json(Themem.get())
+      res.status(200).json(Theme.get())
     } catch (err) {
       manageAllErrors(res, err)
     }
   })
   
-  module.exports = router
+  
+  
+///////////////////////////////////
+//
+//    AFFICHER UN THEME
+//
+///////////////////////////////////
+router.get('/:themeId', (req, res) => {
+  try {
+    res.status(200).json(Theme.getById(req.params.themeId))
+  } catch (err) {
+    manageAllErrors(res, err)
+  }
+})
+
+module.exports = router
