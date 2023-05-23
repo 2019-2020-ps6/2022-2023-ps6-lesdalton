dc59a5717714c2994b091144fbe2c2d9b93c5919
Saad Beidouri <saadbeidouri@Saads-MacBook-Air.local>
2023-05-23 20:58:17 +0200

    debug done

--- back-end/app/models/index.js
+++ back-end/app/models/index.js
@@ -1,11 +1,8 @@
-const Answer = require('./answer.model.js')
-const Question = require('./question.model.js')
+
 const Quiz = require('./quiz.model.js')
 const User = require('./user.model.js')
 
 module.exports = {
-  Answer,
-  Question,
   Quiz,
   User,
 }
