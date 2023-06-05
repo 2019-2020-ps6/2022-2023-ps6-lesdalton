715b6cec9d85c2ea1d5b31e22f37d71c22d6e2b3
Saad Beidouri <saadbeidouri@Saads-MacBook-Air.local>
2023-06-05 11:11:01 +0200

    models answer

---
+++ back-end/app/models/answer.model.js
@@ -0,0 +1,9 @@
+const Joi = require('joi')
+const BaseModel = require('../utils/base-model')
+
+const answerSchema = new BaseModel('Answer', {
+  text: Joi.string().required(),
+  isCorrect: Joi.boolean().required(),
+})
+
+module.exports = answerSchema
--- back-end/app/models/question.model.js
+++ back-end/app/models/question.model.js
@@ -1,7 +1,8 @@
 const Joi = require('joi')
 const BaseModel = require('../utils/base-model.js')
+const Answer = require('./answer.model.js')
 
 module.exports = new BaseModel('Question', {
   text: Joi.string().required(),
-  answers: Joi.array(),
+  answers: Joi.array(Answer.schema),
 })
