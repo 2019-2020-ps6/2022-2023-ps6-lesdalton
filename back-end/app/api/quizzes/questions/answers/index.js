8e00afe07a14ac16b8010c09c046afc117b1c3c8
Jamal Eddine El Betioui <jamal-eddine.el-betioui@etu.unice.fr>
2023-06-05 11:26:07 +0200

    get answers

--- back-end/app/api/quizzes/questions/answers/index.js
+++ back-end/app/api/quizzes/questions/answers/index.js
@@ -1,16 +1,16 @@
 const { Router } = require('express')
-const { Answer } = require('../../../../models')
+const { Answer, Quiz, Question} = require('../../../../models')
 
 const { getQuestionFromQuiz } = require('../manager')
-const { filterAnswersFromQuestion, getAnswerFromQuestion } = require('./manager')
+const { getAnswerFromQuestion } = require('./manager')
 
 const router = new Router({ mergeParams: true })
 
 router.get('/', (req, res) => {
   try {
-    const question = getQuestionFromQuiz(req.params.quizId, req.params.questionId)
-    const answers = filterAnswersFromQuestion(question.id)
-    res.status(200).json(answers)
+    let id = req.params.questionId
+    if (typeof id === 'string') id = parseInt(id, 10)
+    res.status(200).json(Quiz.getById(req.params.quizId).questions.find((item) => item.id === id).answers)
   } catch (err) {
     if (err.name === 'NotFoundError') {
       res.status(404).end()
