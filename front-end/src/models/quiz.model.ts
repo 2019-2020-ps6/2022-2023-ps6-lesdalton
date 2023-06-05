63bb136a3d51cadd47872ec4d1ad270f698f2cc4
AchehbouneMohammedAmine <mohammed-amine.achehboune@etu.univ-cotedazur.fr>
2023-06-05 22:55:34 +0200

    modifs

---
+++ front-end/src/models/quiz.model.ts
@@ -0,0 +1,10 @@
+import {Theme} from "./theme.models";
+import {Question} from "./question.model";
+
+export interface Quiz {
+  id: string;
+  name:string;
+  theme:Theme;
+  questions:Question[];
+
+}
