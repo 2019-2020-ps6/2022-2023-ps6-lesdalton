81ad057ca5cff58bc9ff84195decb6db46788002
AchehbouneMohammedAmine <mohammed-amine.achehboune@etu.univ-cotedazur.fr>
2023-06-06 23:20:01 +0200

    modifs

--- back-end/app/models/answer.model.js
+++ back-end/app/models/answer.model.js
@@ -7,3 +7,51 @@ const answerSchema = new BaseModel('Answer', {
 })
 
 module.exports = answerSchema
+
+const Joi = require('joi')
+const BaseModel = require('../utils/base-model')
+
+const answerSchema = new BaseModel('Answer', {
+  text: Joi.string().required(),
+  isCorrect: Joi.boolean().required(),
+})
+
+module.exports = answerSchema
+
+
+
+const Joi = require('joi')
+const BaseModel = require('../utils/base-model')
+
+const answerSchema = new BaseModel('Answer', {
+  text: Joi.string().required(),
+  isCorrect: Joi.boolean().required(),
+})
+
+module.exports = answerSchema
+
+
+
+const Joi = require('joi')
+const BaseModel = require('../utils/base-model')
+
+const answerSchema = new BaseModel('Answer', {
+  text: Joi.string().required(),
+  isCorrect: Joi.boolean().required(),
+})
+
+module.exports = answerSchema
+
+
+
+const Joi = require('joi')
+const BaseModel = require('../utils/base-model')
+
+const answerSchema = new BaseModel('Answer', {
+  text: Joi.string().required(),
+  isCorrect: Joi.boolean().required(),
+})
+
+module.exports = answerSchema
+
+
