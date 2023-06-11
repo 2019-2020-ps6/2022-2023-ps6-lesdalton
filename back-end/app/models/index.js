68197ea16624da36d5bbeed756b89387bd21fec9
AchehbouneMohammedAmine <mohammed-amine.achehboune@etu.univ-cotedazur.fr>
2023-06-11 23:12:27 +0200

    modif de index model

--- back-end/app/models/index.js
+++ back-end/app/models/index.js
@@ -2,10 +2,14 @@ const Quiz = require('./quiz.model.js')
 const User = require('./user.model.js')
 const Theme = require('./theme.model.js')
 const Question = require('./question.model.js')
+const Answer = require('./answer.model.js')
+const Result = require('./result.model.js')
 
 module.exports = {
   Quiz,
   User,
   Theme,
   Question,
+  Answer,
+  Result
 }
