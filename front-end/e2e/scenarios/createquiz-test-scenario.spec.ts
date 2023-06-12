50aeb8b3c4d1d6234dacf48adb01356d8e43f17d
AchehbouneMohammedAmine <mohammed-amine.achehboune@etu.univ-cotedazur.fr>
2023-06-12 11:33:56 +0200

    ajout du test de creation de quiz

---
+++ front-end/e2e/scenarios/createquiz-test-scenario.spec.ts
@@ -0,0 +1,27 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AddQuizComponent } from 'src/app/quizzes/add-quiz/add-quiz.component';
+
+describe('AddQuizComponent', () => {
+  let component: AddQuizComponent;
+  let fixture: ComponentFixture<AddQuizComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [AddQuizComponent],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AddQuizComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create the component', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should initialize component', () => {
+    expect(component.ngOnInit).toBeTruthy();
+  });
+});
