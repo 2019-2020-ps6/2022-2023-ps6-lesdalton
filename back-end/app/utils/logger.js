5a4dd684762604fbb25ca9240b2c0b78734f4a29
Saad Beidouri <saadbeidouri@Saads-MacBook-Air.local>
2023-06-12 09:11:12 +0200

    fix problems

--- back-end/app/utils/logger.js
+++ back-end/app/utils/logger.js
@@ -1,22 +1,9 @@
-const fs = require('fs');
-const path = require('path');
-
-class Logger {
-  constructor(logFilePath) {
-    this.logFilePath = logFilePath;
-  }
-
-  log(message) {
-    const logMessage = `[${new Date().toISOString()}] ${message}\n`;
-    fs.appendFile(this.logFilePath, logMessage, (err) => {
-      if (err) {
-        console.error('Error writing to log file:', err);
-      }
-    });
-  }
+/* eslint-disable no-console */
+const originalConsoleLog = console.log
+console.log = function consoleLog(...args) {
+  const newArguments = [`[${new Date().toISOString()}]`, ...args]
+  return originalConsoleLog.apply(this, newArguments)
 }
+console.info = console.log
 
-const logFilePath = path.join(__dirname, 'app.log');
-const logger = new Logger(logFilePath);
-
-module.exports = logger;
+module.exports = console
