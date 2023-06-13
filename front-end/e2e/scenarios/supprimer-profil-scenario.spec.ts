90ece1ef7a336835363bb80c88b744d8a53e4643
komi-assimpah <115261320+komi-assimpah@users.noreply.github.com>
2023-06-13 11:13:35 +0200

    delete user test

---
+++ front-end/e2e/scenarios/supprimer-profil-scenario.spec.ts
@@ -0,0 +1,56 @@
+import { test, expect } from '@playwright/test';
+import { testUrl } from 'e2e/e2e.config';
+import { AppFixture } from 'src/app/app.fixture';
+import { UserFixture } from 'src/app/users/user/user.fixture';
+import { UserFormFixture } from 'src/app/users/user-form/user-form.fixture';
+import { text } from 'stream/consumers';
+
+
+
+// test.describe is a hook that creates a test group and lets you define lifecycle stages such as beforeEach.
+test.describe('Supprimer un profil', () => {
+
+    test('Config User', async ({ page }) => {
+        await page.goto(testUrl + '/actions');
+        
+        await page.goto(testUrl + '/actions');
+        const button = await page.locator('button.button-card[routerLink="/password"]');
+        await button.click();
+        await expect(page).toHaveURL("http://localhost:4200/password");
+        const input = await page.locator('input#password');
+        await input.fill('1234');
+        const buttonValider = await page.locator('button.button-card[type="submit"]');
+        await buttonValider.click();
+
+        //create all fixtures
+        const userFormFixture = new UserFormFixture(page);
+        const userFixture = new UserFixture(page);
+
+        await expect(page).toHaveURL("http://localhost:4200/user-list");
+
+        await test.step(`Supprimer user`, async () => {
+
+
+            //const configButtonSelector = '.user-list .card:has(h2:has-text("Alpha Roméo")) button.button-card';
+            const deleteButtonSelector = '.user-list .card button.button-card:nth-child(2)';
+            
+
+            await page.waitForSelector(deleteButtonSelector);
+
+            // Sélectionner le bouton de configuration
+            const deleteButton = await page.$(deleteButtonSelector);
+
+            // Effectuer un clic sur le bouton de configuration
+            if (deleteButton) {
+                await deleteButton.click();
+            }
+
+            await userFormFixture.clickValidateButton();
+
+        });
+
+
+    });
+
+
+});
