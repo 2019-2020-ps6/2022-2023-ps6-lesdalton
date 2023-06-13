b1ca8ae4bbc3b83d94924918c8310cc0fd9a0a63
AchehbouneMohammedAmine <mohammed-amine.achehboune@etu.univ-cotedazur.fr>
2023-06-14 00:07:29 +0200

    verification des parametres d'un profil

--- front-end/e2e/scenarios/detecter-configuration3-scenario.spec.ts
+++ front-end/e2e/scenarios/detecter-configuration3-scenario.spec.ts
@@ -12,3 +12,11 @@ test("Launch the Selectors hub test page", async () => {
   await page.waitForSelector(".dropbtn", {
     state: "visible",
   });
+   // we want to mask this locator
+   const btn = await page.locator(".dropbtn");
+   //await expect(btn).toHaveCSS('background-color','#4CAF50'); // it will fail
+   //await expect(btn).toHaveCSS('background-color','rgb(76,175,80)'); // it will also fail due to spaces
+   await expect(btn).toHaveCSS('background-color','rgb(76, 175, 80)');
+   await page.waitForTimeout(2000);
+   await page.close();
+   
