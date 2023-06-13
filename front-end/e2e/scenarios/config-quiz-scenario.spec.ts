26fb0c025ab95e6a76334fa9aec6c000fa094a9c
Saad Beidouri <saadbeidouri@Saads-MacBook-Air.local>
2023-06-13 14:23:59 +0200

    refactor quiz-config-scenario with fixture

--- front-end/e2e/scenarios/config-quiz-scenario.spec.ts
+++ front-end/e2e/scenarios/config-quiz-scenario.spec.ts
@@ -1,47 +1,55 @@
 import { test, expect } from "@playwright/test";
 import { testUrl } from 'e2e/e2e.config';
-import { User } from "../../src/models/user.models";
+import {QuizCardFixture} from "../../src/app/quizzes/quiz-card/quiz-card.fixture";
+import {QuizConfigFixture} from "../../src/app/quizzes/quiz-config/quiz-config.fixture";
+import {QuizConfigQuestionFixture} from "../../src/app/quizzes/quiz-config-question/quiz-config-question.fixture";
+test.describe('Quiz Feature', () => {
+  test('Configurer un quiz', async ({page}) => {
+    await page.goto(testUrl + '/actions');
 
-test('Configurer un quiz', async ({ page }) => {
-  await page.goto(testUrl +'/actions');
+    const button = await page.locator('button.button-card[routerLink="/password-quiz"]');
+    await button.click();
+    await expect(page).toHaveURL("http://localhost:4200/password-quiz");
+    const input = await page.locator('input#password');
+    await input.fill('1234');
+    const buttonValider = await page.locator('button.button-card[type="submit"]');
+    await buttonValider.click();
 
-  const button = await page.locator('button.button-card[routerLink="/password-quiz"]');
-  await button.click();
-  await expect(page).toHaveURL("http://localhost:4200/password-quiz");
-  const input = await page.locator('input#password');
-  await input.fill('1234');
-  const buttonValider = await page.locator('button.button-card[type="submit"]');
-  await buttonValider.click();
+    await expect(page).toHaveURL("http://localhost:4200/add-quiz");
 
-  await expect(page).toHaveURL("http://localhost:4200/add-quiz");
+    await test.step('Click "Configurer" button', async () => {
+      const quizCardFixture = new QuizCardFixture(page);
+      await quizCardFixture.clickButton('Configurer');
+    });
 
-  const firstDiv = await page.locator('app-quiz-card div').first();
-  const configureButton = await firstDiv.getByRole('button', { name: 'Configurer' });
-  await configureButton.click();
-
-  const inputNvQst=await page.locator('input#text');
-  await inputNvQst.fill('Nouvelle Question');
-  const buttonValiderNvQst = await page.locator('button.button-card[type="submit"]');
-  await buttonValiderNvQst.click();
-
-  const firstButton = await page.locator('.list button.button-card').first();
-  await firstButton.click();
-
-
-
-  const inputNvReponse=await page.locator('input#text');
-  await inputNvReponse.fill('Nouvelle Reponse');
-  const checkbox = await page.locator('input#isCorrect');
-  await checkbox.click();
-  const buttonAjouterRep = await page.locator('button.button-card[type="submit"]');
-  await buttonAjouterRep.click();
-
-  const valider = await page.getByRole('button', { name: 'Valider' });
-  await valider.click();
-  const valider2 = await page.getByRole('button', { name: 'Valider' });
-  await valider2.click();
-
-  await expect(page).toHaveURL("http://localhost:4200/add-quiz");
+    await test.step('Fill input field and click "Valider" button', async () => {
+      const quiConfigFixture=new QuizConfigFixture(page);
+      await quiConfigFixture.fillInput('Nouvelle Question');
+      await quiConfigFixture.clickButton('button.button-card[type="submit"]');
+      await quiConfigFixture.clickOnFirstQst();
+    });
 
 
+
+    await test.step('Fill input field and click "Valider" button', async () => {
+      const quizConfigQstFixture=new QuizConfigQuestionFixture(page);
+      await quizConfigQstFixture.fillInput('Nouvelle Réponse test');
+      await quizConfigQstFixture.clickButton('input#isCorrect');
+      await quizConfigQstFixture.clickButton('button.button-card[type="submit"]')
+    });
+
+
+    await test.step('Validation of the question',async()=>{
+      const valider = await page.getByRole('button', {name: 'Valider'});
+      await valider.click();
+    });
+
+    await test.step('Validation of the quiz',async()=>{
+      const valider2 = await page.getByRole('button', {name: 'Valider'});
+      await valider2.click();
+    });
+
+    await expect(page).toHaveURL("http://localhost:4200/add-quiz");
+
+  });
 });
