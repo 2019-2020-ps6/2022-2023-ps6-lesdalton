5ed9b78f83b00024033021ef49d2267671419db0
Jamal Eddine El Betioui <jamal-eddine.el-betioui@etu.unice.fr>
2023-06-15 19:52:36 +0200

    e2e

--- front-end/src/environment/environment.e2e.ts
+++ front-end/src/environment/environment.e2e.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8000', // URL de l'API backend pour les tests e2e
-  // Autres variables d'environnement spécifiques aux tests e2e
+  apiUrl: 'http://localhost:8000',
 };
