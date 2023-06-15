2f62c5224e978c008070e4ecce7e3359f58a69b7
Saad Beidouri <saadbeidouri@Saads-MacBook-Air.local>
2023-06-15 17:20:37 +0200

    e2e

---
+++ front-end/src/environment/environment.e2e.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:8000', // URL de l'API backend pour les tests e2e
+  // Autres variables d'environnement spécifiques aux tests e2e
+};
